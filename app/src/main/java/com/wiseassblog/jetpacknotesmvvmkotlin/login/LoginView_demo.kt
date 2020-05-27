6113eaec91841287c3a15aa4f0ca49bc2b1fd96d
Devansh <devanshsharma289@gmail.com>
2020-05-27 15:45:41 -0400

    finished working with loginview by adding firebase methods

--- app/src/main/java/com/wiseassblog/jetpacknotesmvvmkotlin/login/LoginView_demo.kt
+++ app/src/main/java/com/wiseassblog/jetpacknotesmvvmkotlin/login/LoginView_demo.kt
@@ -5,15 +5,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.content.Intent
+import android.util.Log
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.wiseassblog.jetpacknotesmvvmkotlin.R
+import com.wiseassblog.jetpacknotesmvvmkotlin.common.RC_SIGN_IN
 import com.wiseassblog.jetpacknotesmvvmkotlin.common.startWithFade
 import com.wiseassblog.jetpacknotesmvvmkotlin.login.buildlogic.LoginInjector
+import com.wiseassblog.jetpacknotesmvvmkotlin.model.LoginResult
 import com.wiseassblog.jetpacknotesmvvmkotlin.note.NoteActivity
 import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_login.view.*
@@ -28,7 +35,7 @@ class LoginView_demo : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.root.fragment_login, container, false)
+        //return inflater.inflate(R.layout.root.fragment_login, container, false)
     }
 
     override fun onStart() {
@@ -64,11 +71,46 @@ class LoginView_demo : Fragment() {
         btn_auth_attempt.setOnClickListener{ viewModel.handleEvent(LoginEvent.OnAuthButtonClick )}
         imb_toolbar_back.setOnClickListener{startListActivity()}
 
-        requireActivity().addOnBackPressedCallback(viewLifecycleOwner,
+        /*requireActivity().addOnBackPressedCallback(viewLifecycleOwner,
             OnBackPressedCallback {
                 startListActivity()
                 true
             }
+        )*/
+    }
+
+    private fun startSignInFlow() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        var userToken: String? = null
+
+        try {
+            val account: GoogleSignInAccount? = task.getResult(ApiException::class.java)
+
+            if (account != null) userToken = account.idToken
+        } catch (exception: Exception) {
+            Log.d("LOGIN", exception.toString())
+        }
+
+        viewModel.handleEvent(
+            LoginEvent.OnGoogleSignInResult(
+                LoginResult(
+                    requestCode,
+                    userToken
+                )
+            )
         )
     }
 
